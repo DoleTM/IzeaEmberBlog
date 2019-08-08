6c2f143b772f4f6a92675a81886b71db1fae42cf
DoleTM <adylantoremember@gmail.com>
2019-08-08 12:23:15 -0400

    Pagination working, still need to fix styling

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,10 +3,11 @@ import { inject } from '@ember/service';
 
 export default Route.extend({
     ajax: inject(),
+    location: 9,
     model() {
-        const post = this.get('ajax')
+        const posts = this.get('ajax')
             .request('https://jsonplaceholder.typicode.com/posts')
             .then(response => response.sort(() => Math.random() - 0.5));
-            return post;
+        return posts;
     }
 });
