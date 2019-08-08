bbcb4c4c973c1fe81eb924b8bdfdcd606b9f3a0a
DoleTM <adylantoremember@gmail.com>
2019-08-08 16:02:49 -0400

    Test modal working

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller';
 
 export default Controller.extend({
-    paginateBy: 9
+    paginateBy: 9,
+    isShowingModal: false,
+
+    actions: {
+        toggleModal: function() {
+            this.toggleProperty('isShowingModal');
+        }
+    }
 });
